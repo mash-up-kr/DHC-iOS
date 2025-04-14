883ace06abb774c775f1066ce4761dc45a35753f
hyerin <hope1053@naver.com>
2025-04-14 20:23:10 +0900

    init: dhc-ios 프로젝트 생성

---
+++ DHC-iOS/DHC_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DHC_iOSApp.swift
+//  DHC-iOS
+//
+//  Created by 최혜린 on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct DHC_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

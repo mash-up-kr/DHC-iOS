1eeabbcfc1ebab345d4ed3c835aa2bd430b2d81c
dbqls200 <dbqls200@gmail.com>
2025-06-09 19:09:00 +0900

    feat: UUID 저장 및 불러오기 로직 구현

---
+++ Project/App/Sources/Util/UUID/UUIDManager.swift
@@ -0,0 +1,29 @@
+//
+//  UUIDManager.swift
+//  ProductName
+//
+//  Created by 김유빈 on 6/9/25.
+//
+
+import Foundation
+
+import ComposableArchitecture
+
+enum UUIDManager {
+  static let uuidKey = "install_uuid"
+
+  @Dependency(\.keychain) static var keychain
+
+  static func saveUUID(_ uuid: UUID) -> Bool {
+    keychain.save(uuidKey, uuid.uuidString)
+  }
+
+  static func loadUUID() -> UUID? {
+    guard let uuidString = keychain.load(uuidKey) else { return nil }
+    return UUID(uuidString: uuidString)
+  }
+
+  static func deleteUUID() -> Bool {
+    keychain.delete(uuidKey)
+  }
+}

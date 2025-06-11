276994dd7c2d2a20517bcf24444b3a90759b66d6
dbqls200 <dbqls200@gmail.com>
2025-06-11 15:49:34 +0900

    rename: UUIDManager → DeviceIDManager, uuidKey → deviceID로 명확한 네이밍 변경

--- Project/App/Sources/Util/UUID/DeviceIDManager.swift
+++ Project/App/Sources/Util/UUID/DeviceIDManager.swift
@@ -1,5 +1,5 @@
 //
-//  UUIDManager.swift
+//  DeviceIDManager.swift
 //  ProductName
 //
 //  Created by 김유빈 on 6/9/25.
@@ -9,21 +9,21 @@ import Foundation
 
 import ComposableArchitecture
 
-enum UUIDManager {
-  static let uuidKey = "install_uuid"
+enum DeviceIDManager {
+  static let deviceID = "install_uuid"
 
   @Dependency(\.keychainClient) static var keychain
 
   static func saveUUID(_ uuid: UUID) -> Bool {
-    keychain.save(uuidKey, uuid.uuidString)
+    keychain.save(deviceID, uuid.uuidString)
   }
 
   static func loadUUID() -> UUID? {
-    guard let uuidString = keychain.load(uuidKey) else { return nil }
+    guard let uuidString = keychain.load(deviceID) else { return nil }
     return UUID(uuidString: uuidString)
   }
 
   static func deleteUUID() -> Bool {
-    keychain.delete(uuidKey)
+    keychain.delete(deviceID)
   }
 }

2c694eff8ed8e8418bb0e2dca468329219f9f356
dbqls200 <dbqls200@gmail.com>
2025-06-13 17:09:12 +0900

    refactor: 명확한 의도 전달을 위해 UUIDManager를 DeviceIDManager로 이름 변경

---
+++ Project/App/Sources/Util/DeviceID/DeviceIDManager.swift
@@ -0,0 +1,51 @@
+//
+//  DeviceIDManager.swift
+//  ProductName
+//
+//  Created by 김유빈 on 6/13/25.
+//
+
+import Foundation
+
+import ComposableArchitecture
+
+@DependencyClient
+struct DeviceIDManager: Sendable {
+  var saveDeviceID: (_ uuid: UUID) throws -> Void
+  var loadDeviceID: () throws -> UUID
+  var deleteDeviceID: () throws -> Void
+}
+
+extension DeviceIDManager: DependencyKey {
+  static let liveValue: DeviceIDManager = {
+    @Dependency(\.keychainClient) var keychainClient
+    let key = "DeviceID"
+
+    return DeviceIDManager(
+      saveDeviceID: { uuid in
+        try keychainClient.save(key, uuid.uuidString)
+      },
+      loadDeviceID: {
+        let uuidString = try keychainClient.load(key)
+        guard let uuid = UUID(uuidString: uuidString) else {
+          throw KeychainError.loadFailed
+        }
+        return uuid
+      },
+      deleteDeviceID: {
+        try keychainClient.delete(key)
+      }
+    )
+  }()
+
+  static var previewValue = Self()
+
+  static var testValue = Self()
+}
+
+extension DependencyValues {
+  var deviceIDManager: DeviceIDManager {
+    get { self[DeviceIDManager.self] }
+    set { self[DeviceIDManager.self] = newValue }
+  }
+}

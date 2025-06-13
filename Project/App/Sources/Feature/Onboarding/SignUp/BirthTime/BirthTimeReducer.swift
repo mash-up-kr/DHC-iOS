75c6e431386ccdcd3af8b928fbd4d01347158d85
hyerin <hope1053@megastudy.net>
2025-06-13 15:20:27 +0900

    IOS-30 feat: BirthTimeView, Reducer 구현

---
+++ Project/App/Sources/Feature/Onboarding/SignUp/BirthTime/BirthTimeReducer.swift
@@ -0,0 +1,61 @@
+//
+//  BirthTimeReducer.swift
+//  ProductName
+//
+//  Created by hyerin on 6/13/25.
+//
+
+import Foundation
+
+import ComposableArchitecture
+
+@Reducer
+struct BirthTimeReducer {
+  init() {}
+
+  @ObservableState
+  struct State: Equatable {
+    var birthTime: Date
+    var isNoIdeaButtonChecked: Bool
+
+    init(
+      birthTime: Date = Date(),
+      isNoIdeaButtonSelected: Bool = false
+    ) {
+      self.birthTime = birthTime
+      self.isNoIdeaButtonChecked = isNoIdeaButtonSelected
+    }
+  }
+
+  enum Action {
+    // View Action
+    case birthTimeChanged(Date)
+    case noIdeaButtonTapped
+    case nextButtonTapped
+    case backButtonTapped
+    
+    // Internal Action
+    
+    // Route Action
+  }
+
+  var body: some Reducer<State, Action> {
+    Reduce { state, action in
+      switch action {
+        case .birthTimeChanged(let birthTime):
+          state.birthTime = birthTime
+          return .none
+          
+        case .noIdeaButtonTapped:
+          state.isNoIdeaButtonChecked.toggle()
+          return .none
+          
+        case .nextButtonTapped:
+          return .none
+          
+        case .backButtonTapped:
+          return .none
+      }
+    }
+  }
+}

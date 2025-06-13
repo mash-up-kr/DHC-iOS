4ae5724dde949c3ffe92f9795efed0896006e2db
hyerin <hope1053@megastudy.net>
2025-06-13 15:37:22 +0900

    IOS-30 feat: IntroInfoView 컴포넌트 구현

---
+++ Project/App/Sources/Design System/View/IntroInfo/IntroInfoView.swift
@@ -0,0 +1,99 @@
+//
+//  IntroInfoView.swift
+//  ProductName
+//
+//  Created by hyerin on 6/13/25.
+//
+
+import SwiftUI
+
+struct IntroInfoView: View {
+  private let style: Style
+  private let title: String
+  private let description: String
+  
+  private let titleColor = ColorResource.Text.main.color
+  private let descriptionColor = ColorResource.Neutral._300.color
+  
+  init(
+    style: Style,
+    title: String,
+    description: String = ""
+  ) {
+    self.style = style
+    self.title = title
+    self.description = description
+  }
+  
+  var body: some View {
+    VStack(alignment: style.contentAlignment, spacing: 16) {
+      Text(title)
+        .textStyle(style.titleFont)
+        .foregroundStyle(titleColor)
+      
+      if !description.isEmpty {
+        Text(description)
+          .textStyle(.body3)
+          .foregroundStyle(descriptionColor)
+      }
+    }
+    .multilineTextAlignment(style.textAlignment)
+    .padding(.top, 24)
+    .padding(.horizontal, 20)
+  }
+}
+
+extension IntroInfoView {
+  enum Style {
+    case intro
+    case page
+    
+    var titleFont: Typography.TypographyStyle {
+      switch self {
+        case .intro:
+          return Typography.Head.h2
+        case .page:
+          return Typography.Head.h1
+      }
+    }
+    
+    var textAlignment: TextAlignment {
+      switch self {
+        case .intro:
+          return .center
+        case .page:
+          return .leading
+      }
+    }
+    
+    var contentAlignment: HorizontalAlignment {
+      switch self {
+        case .intro:
+          return .center
+        case .page:
+          return .leading
+      }
+    }
+  }
+}
+
+#Preview {
+  VStack {
+    IntroInfoView(
+      style: .intro,
+      title: "매일매일 금전운 미션을 통해\n소비습관을 개선해보세요",
+      description: "매일매일 금전운 미션을 통해\n소비습관을 개선해보세요"
+    )
+    
+    IntroInfoView(
+      style: .intro,
+      title: "매일매일 금전운 미션을 통해\n소비습관을 개선해보세요"
+    )
+    
+    IntroInfoView(
+      style: .page,
+      title: "매일매일 금전운 미션을 통해\n소비습관을 개선해보세요",
+      description: "매일매일 금전운 미션을 통해\n소비습관을 개선해보세요"
+    )
+  }
+}

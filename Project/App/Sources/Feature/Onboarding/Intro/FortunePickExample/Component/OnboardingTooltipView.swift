07226192efd2768a42dd956a31119139875103f9
hyerin <hope1053@megastudy.net>
2025-06-20 16:58:49 +0900

    IOS-37 feat: tooltipView 구현

---
+++ Project/App/Sources/Feature/Onboarding/Intro/FortunePickExample/Component/OnboardingTooltipView.swift
@@ -0,0 +1,51 @@
+//
+//  OnboardingTooltipVIew.swift
+//  ProductName
+//
+//  Created by hyerin on 6/20/25.
+//
+
+import SwiftUI
+
+struct OnboardingTooltipView: View {
+  private let message: String
+  
+  init(message: String) {
+    self.message = message
+  }
+  
+  var body: some View {
+    VStack(spacing: 0) {
+      contentView
+      bottomArrowView
+    }
+  }
+  
+  private var contentView: some View {
+    textView
+      .padding(.vertical, 10)
+      .padding(.leading, 10)
+      .padding(.trailing, 12)
+      .background(LinearGradient(.tooltip01))
+      .clipShape(RoundedRectangle(cornerRadius: 8))
+  }
+  
+  private var textView: some View {
+    Text(message)
+      .textStyle(.h7)
+      .foregroundStyle(ColorResource.Text.Highlights.primary.color)
+      .padding(.horizontal, 2)
+  }
+  
+  private var bottomArrowView: some View {
+    BottomRoundedInvertedTriangle(cornerRadius: 1)
+      .fill(
+        LinearGradient(
+          .tooltip01,
+          startPoint: .bottom,
+          endPoint: .top
+        )
+      )
+      .frame(width: 12, height: 6)
+  }
+}

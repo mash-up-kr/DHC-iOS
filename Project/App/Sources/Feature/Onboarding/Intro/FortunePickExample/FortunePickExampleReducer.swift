920a757770704dc63285fd576bdc0cd54444c29e
hyerin <hope1053@megastudy.net>
2025-06-20 18:20:20 +0900

    IOS-37 feat: FortunePickExample Feature 구현

---
+++ Project/App/Sources/Feature/Onboarding/Intro/FortunePickExample/FortunePickExampleReducer.swift
@@ -0,0 +1,45 @@
+//
+//  FortunePickExampleReducer.swift
+//  ProductName
+//
+//  Created by hyerin on 6/17/25.
+//
+
+import ComposableArchitecture
+
+@Reducer
+struct FortunePickExampleReducer {
+  init() {}
+
+  @ObservableState
+  struct State: Equatable {
+    var isCardFlipped: Bool
+
+    init(isCardFlipped: Bool = false) {
+      self.isCardFlipped = isCardFlipped
+    }
+  }
+
+  enum Action {
+    // View Action
+    case cardFlipped
+    case nextButtonTapped
+    
+    // Internal Action
+    
+    // Route Action
+  }
+
+  var body: some Reducer<State, Action> {
+    Reduce { state, action in
+      switch action {
+        case .cardFlipped:
+          state.isCardFlipped = true
+          return .none
+        
+        case .nextButtonTapped:
+          return .none
+      }
+    }
+  }
+}

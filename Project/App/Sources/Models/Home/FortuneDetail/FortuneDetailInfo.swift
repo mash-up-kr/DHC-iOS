9f5124a9e4cd9dcc2bb9679c8080412a7f074696
hyerin <hope1053@naver.com>
2025-06-22 22:35:14 +0900

    IOS-38 feat: FortuneDetailInfo model 선언

---
+++ Project/App/Sources/Models/Home/FortuneDetail/FortuneDetailInfo.swift
@@ -0,0 +1,33 @@
+//
+//  FortuneDetailInfo.swift
+//  ProductName
+//
+//  Created by 최혜린 on 6/22/25.
+//
+
+import SwiftUI
+
+struct FortuneDetailInfo: Equatable {
+  let scoreInfo: FortuneScoreInfo
+  let cardInfo: FortuneCardInfo
+  let detailMessage: String
+  let tipInfos: [TipInfo]
+}
+
+struct FortuneScoreInfo: Equatable {
+  let date: String
+  let score: Int
+  let summary: String
+}
+struct FortuneCardInfo: Equatable {
+  let backgroundImageURL: URL
+  let title: String
+  let fortune: String
+}
+
+struct TipInfo: Equatable, Hashable {
+  let imageURL: URL
+  let title: String
+  let content: String
+  let contentColor: Color?
+}

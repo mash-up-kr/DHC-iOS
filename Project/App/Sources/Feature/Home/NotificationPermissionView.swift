4a80ce7e77260e347e3e0820cbaf020fb6fc55da
dbqls200 <dbqls200@gmail.com>
2025-06-23 02:51:51 +0900

    IOS-36 feat: 푸시 알림 권한 요청 뷰 작업
    - NotificationPermissionView 레이아웃 구성
    - [IOS-25] BottomSheet 컴포넌트 머지 후 주석 해제 예정

---
+++ Project/App/Sources/Feature/Home/NotificationPermissionView.swift
@@ -0,0 +1,45 @@
+//
+//  NotificationPermissionView.swift
+//  ProductName
+//
+//  Created by 김유빈 on 6/17/25.
+//
+
+import SwiftUI
+
+import ComposableArchitecture
+
+struct NotificationPermissionView: View {
+  let store: StoreOf<NotificationPermissionReducer>
+
+  init(store: StoreOf<NotificationPermissionReducer>) {
+    self.store = store
+  }
+
+  var body: some View {
+    // TODO: [IOS-25] BottomSheet 컴포넌트 PR 머지되면 주석 해제하기
+//    DHCBottomSheetContent(
+//      configuration: BottomSheetConfiguration(
+//        title: "알림 설정을\n허용해주세요",
+//        description: "서비스를 원활히 진행하기 위해서\n알림설정이 꼭 필요해요",
+//        showCloseButton: true,
+//        interactiveDisabled: true,
+//        firstButton: .init(
+//          title: "알림 허용하기",
+//          action: {
+//            store.send(.allowButtonTapped)
+//          }
+//        )
+//      )
+//    )
+  }
+}
+
+#Preview {
+  NotificationPermissionView(
+    store: Store(
+      initialState: .init(),
+      reducer: NotificationPermissionReducer.init
+    )
+  )
+}

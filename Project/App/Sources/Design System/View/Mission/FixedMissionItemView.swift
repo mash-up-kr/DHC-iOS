e2c3ecd44e3435de94525f7ce307dcffcb6da2ad
dbqls200 <dbqls200@gmail.com>
2025-06-26 13:21:17 +0900

    IOS-27 feat: 고정 소비습관 미션용 FixedMissionItemView 추가

---
+++ Project/App/Sources/Design System/View/Mission/FixedMissionItemView.swift
@@ -0,0 +1,42 @@
+//
+//  FixedMissionItemView.swift
+//  Flifin
+//
+//  Created by 김유빈 on 6/26/25.
+//
+
+import SwiftUI
+
+// TODO: 디자인팀 답변 오면, 고정 미션 끝나는 당일 표현 방식 적용하기 (D-day? D-0?)
+struct FixedMissionItemView: View {
+  let missionTitle: String
+  let remainingDays: Int
+  @Binding var isMissionCompleted: Bool
+
+  var body: some View {
+    MissionItem(
+      missionTitle: missionTitle,
+      isPinned: true,
+      isMissionCompleted: $isMissionCompleted
+    ) {
+      DHCBadge(
+        badgeTitle: "D-\(remainingDays)",
+        badgeStyle: .spendType
+      )
+    }
+  }
+}
+
+#Preview {
+  FixedMissionItemView(
+    missionTitle: "텀블러 들고 다니기",
+    remainingDays: 14,
+    isMissionCompleted: .constant(true)
+  )
+
+  FixedMissionItemView(
+    missionTitle: "텀블러 들고 다니기",
+    remainingDays: 0,
+    isMissionCompleted: .constant(false)
+  )
+}

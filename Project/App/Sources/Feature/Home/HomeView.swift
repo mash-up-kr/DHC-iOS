96e7e4eac752cea674e0d3d4f201f421f72bc565
dbqls200 <dbqls200@gmail.com>
2025-06-30 20:17:17 +0900

    IOS-45 feat: 홈 화면 레이아웃 구성

--- Project/App/Sources/Feature/Home/HomeView.swift
+++ Project/App/Sources/Feature/Home/HomeView.swift
@@ -13,6 +13,85 @@ struct HomeView: View {
   @Bindable var store: StoreOf<HomeReducer>
 
   var body: some View {
-    Text("Home")
+    ScrollView {
+      VStack(spacing: 0) {
+        headerSection
+
+        Rectangle().frame(width: 144, height: 200)
+          // TODO: card 넣기
+          .frame(maxWidth: .infinity)
+          .padding([.horizontal, .top], 20)
+          .padding(.bottom, 60)
+
+        MissionListView()
+      }
+    }
+    .radialGradientBackground(type: .backgroundGradient02)
   }
+
+  // MARK: 상단 타이틀 섹션
+  private var headerSection: some View {
+    VStack(alignment: .leading, spacing: 16) {
+      Text(todayDateString)
+        .textStyle(.body3)
+        .foregroundStyle(ColorResource.Neutral._300.color)
+
+      HStack(alignment: .top, spacing: 0) {
+        VStack(alignment: .leading, spacing: 12) {
+          Text("지갑이 들뜨는 날,\n한 템포 쉬어가요.") // TODO: 타이틀 연결
+            .textStyle(.h2)
+            .foregroundStyle(ColorResource.Text.Body.primary.color)
+
+          seeMoreButton
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+
+        Button {
+          // TODO: '오늘의 금전운' 화면으로 이동하기
+        } label: {
+          FortuneCoreView(score: 35) // TODO: 점수 연결
+        }
+      }
+    }
+    .padding(.horizontal, 20)
+  }
+
+  private var seeMoreButton: some View {
+    Button {
+      // TODO: '오늘의 금전운' 화면으로 이동
+    } label: {
+      HStack(spacing: 4) {
+        Text("더보기")
+          .textStyle(.body6)
+
+        Image(ImageResource.Chevron.right)
+          .resizable()
+          .renderingMode(.template)
+          .frame(width: 20, height: 20)
+      }
+      .padding(.horizontal, 8)
+      .padding(.vertical, 4)
+      .foregroundStyle(ColorResource.Neutral._300.color)
+      .background {
+        RoundedRectangle(cornerRadius: .infinity)
+          .foregroundStyle(ColorResource.Neutral._700.color)
+      }
+    }
+  }
+
+  private var todayDateString: String {
+    let formatter = DateFormatter()
+    formatter.locale = Locale(identifier: "ko_KR")
+    formatter.dateFormat = "M월 d일"
+    return formatter.string(from: Date())
+  }
+}
+
+#Preview {
+  HomeView(
+    store: Store(
+      initialState: .init(),
+      reducer: HomeReducer.init
+    )
+  )
 }

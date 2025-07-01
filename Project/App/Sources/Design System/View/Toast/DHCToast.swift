cf00dd9b4176e05181060d1d472b188c18952792
dbqls200 <dbqls200@gmail.com>
2025-07-01 18:15:20 +0900

    IOS-51 feat: Toast 컴포넌트 작업

---
+++ Project/App/Sources/Design System/View/Toast/DHCToast.swift
@@ -0,0 +1,32 @@
+//
+//  DHCToast.swift
+//  Flifin
+//
+//  Created by 김유빈 on 7/1/25.
+//
+
+import SwiftUI
+
+struct DHCToast: View {
+  private let message: String
+
+  init(
+    message: String
+  ) {
+    self.message = message
+  }
+
+  var body: some View {
+    Toast(
+      message: message,
+      backgroundColor: ColorResource.Neutral._500.color,
+      cornerRadius: 12,
+      textStyle: Typography.Body.body4,
+      textColor: ColorResource.Text.main.color
+    )
+  }
+}
+
+#Preview {
+  DHCToast(message: "차근차근 잘하고 있어요!")
+}

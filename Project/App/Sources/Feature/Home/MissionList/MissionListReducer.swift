d15ee5eac7da701a90a8e2a655bfa04146d09151
dbqls200 <dbqls200@gmail.com>
2025-07-03 15:00:20 +0900

    IOS-58 feat: 미션 완료 여부 토글 및 서버 상태 업데이트 로직을 위한 MissionReducer 추가

--- Project/App/Sources/Feature/Home/MissionList/MissionListReducer.swift
+++ Project/App/Sources/Feature/Home/MissionList/MissionListReducer.swift
@@ -9,16 +9,21 @@ import ComposableArchitecture
 
 @Reducer
 struct MissionListReducer {
+  @Dependency(\.missionClient) var missionClient
+
   init() {}
 
   @ObservableState
   struct State: Equatable {
     var isTooltipVisible = false
-    
+
     var longTermMission: HomeInfo.Mission
     var todayDailyMissionList: [HomeInfo.Mission] = []
-    
-    init(longTermMission: HomeInfo.Mission, todayDailyMissionList: [HomeInfo.Mission]) {
+
+    init(
+      longTermMission: HomeInfo.Mission,
+      todayDailyMissionList: [HomeInfo.Mission]
+    ) {
       self.longTermMission = longTermMission
       self.todayDailyMissionList = todayDailyMissionList
     }
@@ -27,10 +32,13 @@ struct MissionListReducer {
   enum Action {
     // View Action
     case tooltipTapped
+    case longTermMissionTapped
+    case dailyMissionTapped(missionID: String)
 
     // Internal Action
     case updateLongTermMission(HomeInfo.Mission)
     case updateDailyMissions([HomeInfo.Mission])
+    case missionStatusUpdateResult(Result<String, Error>)
 
     // Route Action
   }
@@ -41,13 +49,67 @@ struct MissionListReducer {
       case .tooltipTapped:
         state.isTooltipVisible.toggle()
         return .none
-        
+
       case .updateLongTermMission(let mission):
         state.longTermMission = mission
         return .none
 
       case .updateDailyMissions(let missions):
         state.todayDailyMissionList = missions
+        return .none
+
+      case .longTermMissionTapped:
+        state.longTermMission.finished.toggle()
+
+        let mission = state.longTermMission
+
+        return .run { send in
+          do {
+            try await missionClient.updateMissionStatus(
+              mission.missionId,
+              mission.finished
+            )
+            await send(
+              .missionStatusUpdateResult(
+                .success(mission.missionId)
+              )
+            )
+          } catch {
+            await send(
+              .missionStatusUpdateResult(
+                .failure(error)
+              )
+            )
+          }
+        }
+
+      case .dailyMissionTapped(let missionID):
+        guard let index = state.todayDailyMissionList
+          .firstIndex(
+            where: {
+              $0.missionId == missionID
+            }
+          )
+        else {
+          return .none
+        }
+
+        state.todayDailyMissionList[index].finished.toggle()
+        let updatedMission = state.todayDailyMissionList[index]
+
+        return .run { send in
+          do {
+            try await missionClient.updateMissionStatus(updatedMission.missionId, updatedMission.finished)
+            await send(.missionStatusUpdateResult(.success(updatedMission.missionId)))
+          } catch {
+            await send(.missionStatusUpdateResult(.failure(error)))
+          }
+        }
+
+      case .missionStatusUpdateResult(let result):
+        if case .failure(let error) = result {
+          print(error)
+        }
         return .none
       }
     }

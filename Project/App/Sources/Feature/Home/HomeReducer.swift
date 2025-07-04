4b6a6358c363b6a4ed0d90940fb90cd2c01dec3c
hyerin <hope1053@naver.com>
2025-07-05 01:42:50 +0900

    IOS-67 feat: ViewState를 통해 화면 상태 관리하도록 구현

--- Project/App/Sources/Feature/Home/HomeReducer.swift
+++ Project/App/Sources/Feature/Home/HomeReducer.swift
@@ -15,6 +15,13 @@ struct HomeReducer {
 
   @ObservableState
   struct State: Equatable {
+    enum ViewState {
+      case firstLaunch
+      case home
+    }
+    
+    var viewState: ViewState = .home
+    
     var path = StackState<Path.State>()
     var missionList = MissionListReducer.State(
       longTermMission: HomeInfo.sample.longTermMission,
@@ -65,8 +72,14 @@ struct HomeReducer {
     Reduce { state, action in
       switch action {
       case .onAppear:
+        if state.isFirstLaunchOfToday {
+          state.viewState = .firstLaunch
+        } else {
+          state.viewState = .home
+        }
+        
         return .send(.fetchHomeData)
-
+        
       case .fetchHomeData:
         return .run { send in
           do {
@@ -76,18 +89,39 @@ struct HomeReducer {
             await send(.homeDataFailed(error))
           }
         }
-
+        
       case .homeDataResponse(let homeInfo):
-        state.homeInfo = homeInfo
-        return .merge(
-          .send(.missionList(.updateLongTermMission(homeInfo.longTermMission))),
-          .send(.missionList(.updateDailyMissions(homeInfo.todayDailyMissionList)))
-        )
-
+        if state.isFirstLaunchOfToday {
+          let dailyFortune = homeInfo.todayDailyFortune
+          state.fortuneLoadingComplete = .init(
+            scoreInfo: .init(
+              date: dailyFortune.date,
+              scoreString: "\(dailyFortune.score)점",
+              score: dailyFortune.score,
+              summary: dailyFortune.fortuneTitle
+            ),
+            cardInfo: .init(
+              backgroundImageURL: .urlForResource(.fortuneCardFrontDefaultView),
+              title: "최고의 날",
+              fortune: "네잎클로버"
+            )
+          )
+          
+          state.viewState = .firstLaunch
+          return .none
+        } else {
+          state.homeInfo = homeInfo
+          return .merge(
+            .send(.missionList(.updateLongTermMission(homeInfo.longTermMission))),
+            .send(.missionList(.updateDailyMissions(homeInfo.todayDailyMissionList)))
+          )
+        }
+        
       case .homeDataFailed:
         return .none
-
-      case .missionList, .fortuneLoadingComplete:
+        
+      case .missionList,
+          .fortuneLoadingComplete:
         return .none
         
       case .moveToFortuneDetail:

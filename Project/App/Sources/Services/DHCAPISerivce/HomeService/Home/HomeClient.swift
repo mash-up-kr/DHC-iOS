61a7b3b2db18aabd85abc976870efbc7ac745aa4
hyerin <hope1053@megastudy.net>
2025-07-04 14:11:26 +0900

    IOS-68 feat: 금전운 상세 API 선언

--- Project/App/Sources/Services/DHCAPISerivce/HomeService/Home/HomeClient.swift
+++ Project/App/Sources/Services/DHCAPISerivce/HomeService/Home/HomeClient.swift
@@ -12,6 +12,7 @@ import ComposableArchitecture
 @DependencyClient
 struct HomeClient {
   var fetchHomeInfo: () async throws -> HomeInfo
+  var fetchFortuneDetail: (_ date: String) async throws -> FortuneDetail
 }
 
 extension HomeClient: DependencyKey {
@@ -24,12 +25,48 @@ extension HomeClient: DependencyKey {
           .request(HomeAPI.home)
           .map(to: HomeDTO.self)
           .toDomain()
+      },
+      fetchFortuneDetail: { date in
+        let endPoint = HomeAPI.fortuneDetail(date: date)
+        
+        do {
+          let response = try await networkManager
+            .request(endPoint)
+            .map(to: FortuneDetailDTO.self)
+            .toDomain
+          return response
+        } catch {
+          if let networkManageError = error as? NetworkManagerError,
+             case .requestFailed(let error, let statusCode) = networkManageError {
+            if statusCode == 404 {
+              throw HomeClientError(code: .needAPIRecall)
+            }
+          }
+          
+          throw HomeClientError(code: .invalidResponse)
+        }
       }
     )
   }()
 
   static let previewValue = HomeClient(
-    fetchHomeInfo: { .sample }
+    fetchHomeInfo: { .sample },
+    fetchFortuneDetail: { _ in
+      .init(
+        scoreInfo: .init(
+          date: "",
+          score: "",
+          summary: ""
+        ),
+        cardInfo: .init(
+          backgroundImageURL: .urlForResource(.fortuneCardFrontDefaultView),
+          title: "",
+          fortune: ""
+        ),
+        detailMessage: "",
+        tipInfos: []
+      )
+    }
   )
   static let testValue = Self()
 }
@@ -38,5 +75,15 @@ extension DependencyValues {
   var homeClient: HomeClient {
     get { self[HomeClient.self] }
     set { self[HomeClient.self] = newValue }
+  }
+}
+
+public struct HomeClientError: Error {
+  public var code: APIResponseError
+  public var underlying: Error?
+
+  public enum APIResponseError: Int {
+    case needAPIRecall
+    case invalidResponse
   }
 }

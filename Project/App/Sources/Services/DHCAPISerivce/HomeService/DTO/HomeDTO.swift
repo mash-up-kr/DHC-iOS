570ea8dda677879f989a0d6e93081fca6086c219
dbqls200 <dbqls200@gmail.com>
2025-07-10 11:31:00 +0900

    IOS-69 refactor: Home API 응답 구조 변경에 따른 DTO 및 도메인 모델 수정

--- Project/App/Sources/Services/DHCAPISerivce/HomeService/DTO/HomeDTO.swift
+++ Project/App/Sources/Services/DHCAPISerivce/HomeService/DTO/HomeDTO.swift
@@ -9,22 +9,41 @@ import Foundation
 
 struct HomeDTO: Decodable {
   let longTermMission: MissionInfo
-  let todayDailyMissionList: [MissionInfo]
-  let todayDailyFortune: DailyFortuneInfo
-  let todayDone: Bool
+  let dailyMissionList: [MissionInfo]
+  let todayFortune: DailyFortuneInfo
+  let isTodayMissionDone: Bool
+  
+  enum CodingKeys: String, CodingKey {
+    case longTermMission
+    case dailyMissionList = "todayDailyMissionList"
+    case todayFortune = "todayDailyFortune"
+    case isTodayMissionDone = "todayDone"
+  }
 }
 
 extension HomeDTO {
   struct MissionInfo: Decodable {
-    let missionId: String
+    let missionID: String
     let category: String
     let difficulty: Int
     let type: String
-    let finished: Bool
+    let isFinished: Bool
     let cost: String
     let endDate: String
     let title: String
     let switchCount: Int
+    
+    enum CodingKeys: String, CodingKey {
+      case missionID = "missionId"
+      case category
+      case difficulty
+      case type
+      case isFinished = "finished"
+      case cost
+      case endDate
+      case title
+      case switchCount
+    }
   }
 }
 
@@ -35,13 +54,22 @@ extension HomeDTO {
     let fortuneDetail: String
     let jinxedColor: String
     let jinxedColorHex: String
+    let jinxedColorImage: String
     let jinxedMenu: String
+    let jinxedMenuImage: String
     let jinxedNumber: Int
     let luckyColor: String
     let luckyColorHex: String
+    let luckyColorImage: String
     let luckyNumber: Int
-    let score: Int
+    let positiveScore: Int
+    let negativeScore: Int
     let todayMenu: String
+    let todayMenuImage: String
+    let score: Int
+    let luckyColorType: String
+    let jinxedColorType: String
+    let fortuneCardImage: String
     
     enum CodingKeys: String, CodingKey {
       case date
@@ -49,13 +77,22 @@ extension HomeDTO {
       case fortuneDetail
       case jinxedColor
       case jinxedColorHex
+      case jinxedColorImage
       case jinxedMenu
+      case jinxedMenuImage
       case jinxedNumber
       case luckyColor
       case luckyColorHex
+      case luckyColorImage
       case luckyNumber
-      case score = "totalScore"
+      case positiveScore
+      case negativeScore
       case todayMenu
+      case todayMenuImage
+      case score = "totalScore"
+      case luckyColorType
+      case jinxedColorType
+      case fortuneCardImage
     }
   }
 }
@@ -64,44 +101,53 @@ extension HomeDTO {
   func toDomain() -> HomeInfo {
     HomeInfo(
       longTermMission: .init(
-        missionId: longTermMission.missionId,
+        id: longTermMission.missionID,
         category: longTermMission.category,
         difficulty: longTermMission.difficulty,
         type: longTermMission.type,
-        finished: longTermMission.finished,
+        isFinished: longTermMission.isFinished,
         cost: longTermMission.cost,
         endDate: longTermMission.endDate,
         title: longTermMission.title,
         switchCount: longTermMission.switchCount
       ),
-      todayDailyMissionList: todayDailyMissionList.map {
+      dailyMissionList: dailyMissionList.map {
         .init(
-          missionId: $0.missionId,
+          id: $0.missionID,
           category: $0.category,
           difficulty: $0.difficulty,
           type: $0.type,
-          finished: $0.finished,
+          isFinished: $0.isFinished,
           cost: $0.cost,
           endDate: $0.endDate,
           title: $0.title,
           switchCount: $0.switchCount
         )
       },
-      todayDailyFortune: .init(
-        date: todayDailyFortune.date,
-        fortuneTitle: todayDailyFortune.fortuneTitle,
-        fortuneDetail: todayDailyFortune.fortuneDetail,
-        jinxedColor: todayDailyFortune.jinxedColor,
-        jinxedColorHex: todayDailyFortune.jinxedColorHex,
-        jinxedMenu: todayDailyFortune.jinxedMenu,
-        jinxedNumber: todayDailyFortune.jinxedNumber,
-        luckyColor: todayDailyFortune.luckyColor,
-        luckyColorHex: todayDailyFortune.luckyColorHex,
-        luckyNumber: todayDailyFortune.luckyNumber,
-        score: todayDailyFortune.score,
-        todayMenu: todayDailyFortune.todayMenu
+      dailyFortune: .init(
+        date: todayFortune.date,
+        fortuneTitle: todayFortune.fortuneTitle,
+        fortuneDetail: todayFortune.fortuneDetail,
+        jinxedColor: todayFortune.jinxedColor,
+        jinxedColorHex: todayFortune.jinxedColorHex,
+        jinxedColorImage: todayFortune.jinxedColorImage,
+        jinxedMenu: todayFortune.jinxedMenu,
+        jinxedMenuImage: todayFortune.jinxedMenuImage,
+        jinxedNumber: todayFortune.jinxedNumber,
+        luckyColor: todayFortune.luckyColor,
+        luckyColorHex: todayFortune.luckyColorHex,
+        luckyColorImage: todayFortune.luckyColorImage,
+        luckyNumber: todayFortune.luckyNumber,
+        positiveScore: todayFortune.positiveScore,
+        negativeScore: todayFortune.negativeScore,
+        todayMenu: todayFortune.todayMenu,
+        todayMenuImage: todayFortune.todayMenuImage,
+        score: todayFortune.score,
+        luckyColorType: todayFortune.luckyColorType,
+        jinxedColorType: todayFortune.jinxedColorType,
+        fortuneCardImage: todayFortune.fortuneCardImage
       ),
-      todayDone: todayDone
+      isTodayMissionDone: isTodayMissionDone
     )
   }
 }

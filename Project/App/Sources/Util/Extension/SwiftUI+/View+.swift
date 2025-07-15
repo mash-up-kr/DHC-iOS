7513d30b837d7134892ce641a3e4b6737b59c122
dbqls200 <dbqls200@gmail.com>
2025-07-16 08:44:43 +0900

    IOS-50 feat: List에 적용되던 여러 Modifier를 View Extension에서 하나로 통합

--- Project/App/Sources/Util/Extension/SwiftUI+/View+.swift
+++ Project/App/Sources/Util/Extension/SwiftUI+/View+.swift
@@ -16,3 +16,20 @@ extension View {
     }
   }
 }
+
+extension View {
+  func plainListRow() -> some View {
+    self
+      .listRowSeparator(.hidden)
+      .listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 0))
+      .listRowBackground(ColorResource.Background.main.color)
+      .background(ColorResource.Background.main.color)
+  }
+
+  func plainListBackground() -> some View {
+    self
+      .listStyle(.plain)
+      .scrollContentBackground(.hidden)
+      .background(ColorResource.Background.main.color)
+  }
+}

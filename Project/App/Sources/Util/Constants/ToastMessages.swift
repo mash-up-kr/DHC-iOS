bca9f1b2009ff2c51daaf1d3507b38650d41b5c7
dbqls200 <dbqls200@gmail.com>
2025-07-17 09:57:51 +0900

    IOS-75 feat: 미션 완료 시 노출될 Toast 메시지 Enum 추가

---
+++ Project/App/Sources/Util/Constants/ToastMessages.swift
@@ -0,0 +1,16 @@
+//
+//  ToastMessages.swift
+//  Flifin
+//
+//  Created by 김유빈 on 7/17/25.
+//
+
+enum ToastMessages {
+  static let mission = [
+    "차근차근 잘하고 있어요!",
+    "시작이 반이죠! 잘하고 있어요",
+    "멋져요! 오늘도 발전하는 하루에요",
+    "점점 부자가 되고 있어요!",
+    "작은 절약이 큰 변화를 만들어요",
+  ]
+}

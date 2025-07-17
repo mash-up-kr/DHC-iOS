5393ebf7b8fba7657a530faecf2a8d271a8fcb20
dbqls200 <dbqls200@gmail.com>
2025-07-17 14:33:18 +0900

    IOS-106 design: 미션 완료 Popup 내부 이미지 size, padding 값 조정

--- Project/App/Sources/Feature/Home/Component/HomePopup.swift
+++ Project/App/Sources/Feature/Home/Component/HomePopup.swift
@@ -53,7 +53,8 @@ struct HomePopup: View {
 
       ImageResource.celebration.image
         .resizable()
-        .scaledToFit()
+        .frame(width: 161, height: 138)
+        .padding(.horizontal, 69.5)
 
       VStack(spacing: 4) {
         CTAButton(
